602ef05f1b61a2cd17a1a2084686d36246d672c6
todosparaunoSPE <148250432+todosparaunoSPE@users.noreply.github.com>
2023-11-23 14:21:48 -0600

    Add files via upload

---
+++ futbol.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 23 13:45:08 2023
+
+@author: jperezr
+"""
+
+import pandas as pd
+import streamlit as st
+from mitosheet.streamlit.v1 import spreadsheet
+
+# Create a dataframe with pandas (you can pass any pandas dataframe)
+
+
+
+#dataframe = pd.DataFrame({'A': [1, 2, 3]})
+
+dataframe = pd.read_csv("Libro1.csv")
+
+# Display the dataframe in a Mito spreadsheet
+final_dfs, code = spreadsheet(dataframe)
+
+# Display the final dataframes created by editing the Mito component
+# This is a dictionary from dataframe name -> dataframe
+#st.write(final_dfs)   PARA QUE SE VEA EL CÓDIGO EN LA PÁGINA WEB
+
+# Display the code that corresponds to the script
+#st.code(code)   PARA QUE SE VEA EL CÓDIGO EN LA PÁGINA WEB
